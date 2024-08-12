e2890853fb0cb1a11bc7283315f57826dc0469b0
Ana <anaanituss@gmail.com>
2024-08-12 16:17:52 -0300

    Versión inicial

---
+++ JS/script.js
@@ -0,0 +1,118 @@
+
+/*-  navBar toggle  -*/
+let navBar = document.querySelector('.navLink');
+let menuBar = document.querySelector('#menuBtn');
+
+menuBar.onclick = () => {
+    navBar.classList.toggle('active');
+}
+
+
+
+
+/* ++ scroll section and sticky navbar ++ */
+
+window.onscroll = () => {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+}
+
+
+
+/* swiper js */
+var swiper = new Swiper(".myHome", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+/* counter section js */
+
+let valueDisplay = document.querySelectorAll('.num');
+let interval = 1000;
+
+valueDisplay.forEach((valueDisplay) => {
+
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-value"));
+
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(function () {
+
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if(startValue == endValue){
+        clearInterval (counter);
+      }
+    }, duration);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
